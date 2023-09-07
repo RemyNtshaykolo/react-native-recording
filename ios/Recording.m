45db70061ee666441115d010cc3adcc8c1dce361
Rémy Ntshaykolo <remy.nts@gmail.com>
2023-09-07 11:47:12 +0200

    wip: master

--- ios/Recording.m
+++ ios/Recording.m
@@ -24,7 +24,6 @@ RCT_EXPORT_METHOD(init:(NSDictionary *) options) {
     NSError *error = nil;
     [session setCategory:AVAudioSessionCategoryPlayAndRecord
                     mode:AVAudioSessionModeVoiceChat
-                 options:AVAudioSessionCategoryOptionDefaultToSpeaker|AVAudioSessionCategoryOptionAllowBluetoothA2DP
                    error:&error];
 
 
