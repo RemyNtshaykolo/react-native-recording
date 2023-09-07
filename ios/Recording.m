d05d8ba1d7739391110834460f4d0912d65e0b59
Rémy Ntshaykolo <remy.nts@gmail.com>
2023-09-07 12:11:41 +0200

    wip: master

--- ios/Recording.m
+++ ios/Recording.m
@@ -24,7 +24,7 @@ RCT_EXPORT_METHOD(init:(NSDictionary *) options) {
     NSError *error = nil;
     [session setCategory:AVAudioSessionCategoryRecord
                     mode:AVAudioSessionModeMeasurement
-                 options:AVAudioSessionCategoryOptionDefaultToSpeaker
+                 options:AVAudioSessionCategoryOptionDefaultToSpeaker|AVAudioSessionCategoryOptionMixWithOthers
                    error:&error];
 
 
